4453737db940e61b6bb5e68be410afb5dffcf099
LucaBourke7 <A00031454@myTUDublin.ie>
2026-04-13 13:07:12 +0100

    Changed labels and added a smiley face

--- main.gd
+++ main.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
-var num_faces = 10
+func draw_face():
+	var num_faces= randi_range(20,150)
+	draw_circle(Vector2(700, 200), 10, Color.RED, false, 200,)
+	draw_circle(Vector2(650, 175), 10, Color.BLUE, true, 10,)
+	draw_circle(Vector2(750, 175), 10, Color.BLUE, true, 10,)
+	
+		
+	
 
-var face_size = randi_range(20,150)
+func _draw():
+	draw_face()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var face_size = randi_range(20,150)
+	draw_circle(Vector2(50, 50), 100, Color.BLANCHED_ALMOND, false, 10)
 	pass # Replace with function body.
 
 func _draw_face():
@@ -14,12 +24,9 @@ func _draw_face():
 	var num_eyes = randi_range(2,6) # Pick random amount from 2 to 6
 	
 	for i in range(num_faces):
-		var x = randi_range(0,1000)
-		var y = randi_range(-1000,1000)
-		var r = randi_range(1,10)
-		draw_circle(Vector2(x,y), r, Color.BLUE)
+		draw_circle(Vector2(face_size,face_size), 5, Color.BLUE)
 
-func _on_button_pressed():
+func _on_button_pressed() -> void:
 	_draw_face()
 
 
