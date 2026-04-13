7f4e2f3bfcee7655e004a29fd103142a49b06041
LucaBourke7 <A00031454@myTUDublin.ie>
2026-04-13 12:31:55 +0100

    Add random values for x and y position and the radius

--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var num_circles = 10
+var num_faces = 10
 
 var face_size = randi_range(20,150)
 
@@ -9,9 +9,15 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _draw_face:
+	var num_faces = randi_range(1,10) # Pick random number of faces to draw from 1 to 10
 	var face_size = randi_range(20,150) # Pick random face size from 20 to 150
 	var num_eyes = randi_range(2,6) # Pick random amount from 2 to 6
-	draw_circle(Vector2(x,y), r, Color.BLUE)
+	
+	for i in range(num_faces):
+		var x = randi_range(0,1000)
+		var y = randi_range(-1000,1000)
+		var r = randi_range(1,10)
+		draw_circle(Vector2(x,y), r, Color.BLUE)
 
 #func _on_button_pressed:
 	
